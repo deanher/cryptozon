20ec0bd135fdffc6c7cbe9f8cbef1b5c0f8f5ae3
Dean Herringer <deanher@gmail.com>
2019-07-24 21:54:57 +0200

    disable https

--- api/Cryptozon.Api/Startup.cs
+++ api/Cryptozon.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
@@ -21,13 +22,13 @@ namespace Cryptozon.Api
     public IConfiguration Configuration { get; }
     public IHostingEnvironment HostingEnvironment { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       var appSettings = Configuration.Get<AppSettings>();
 
       services.Configure<AppSettings>(Configuration);
       ConfigureLogging(appSettings.Logging, HostingEnvironment);
+
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
 
@@ -46,20 +47,13 @@ namespace Cryptozon.Api
       Log.Logger = logConfig.CreateLogger();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
-      else
-      {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
 
-      app.UseHttpsRedirection();
       app.UseMvc();
     }
   }
