a8c9113ed1274db488135ae48903b3d149f51014
Dean Herringer <deanher@gmail.com>
2019-07-26 06:33:10 +0200

    add `CoinMarketCap` details to settings

--- api/Cryptozon.Api/AppSettings.cs
+++ api/Cryptozon.Api/AppSettings.cs
@@ -2,7 +2,21 @@
 {
   public class AppSettings
   {
+    public ExternalService CoinMarketCap { get; set; } 
     public Logging Logging { get; set; }
     public string[] AllowedHosts { get; set; }
+  }
+
+  public class ExternalService
+  {
+    public string Key { get; set; }
+    public string BaseUrl { get; set; }
+  }
+
+  public class Logging
+  {
+    public string Level { get; set; }
+    public string OutputTemplate { get; set; }
+    public string OutputPath { get; set; }
   }
 }
