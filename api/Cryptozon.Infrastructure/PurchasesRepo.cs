0a7444695265c183609f7bcee8b1f62fe2aac972
Dean Herringer <deanher@gmail.com>
2019-07-27 23:17:17 +0200

    cleanup usings

--- api/Cryptozon.Infrastructure/PurchasesRepo.cs
+++ api/Cryptozon.Infrastructure/PurchasesRepo.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cryptozon.Domain;
 using Cryptozon.Domain.Purchases;
 using Dapper;
 
--- api/Cryptozon.Infrastructure/Quote.cs
+++ api/Cryptozon.Infrastructure/Quote.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Cryptozon.Infrastructure
 {
   public class Quote
