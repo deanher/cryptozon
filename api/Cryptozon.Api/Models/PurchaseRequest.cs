6e7db275a1def0d3ecfeeb9c23790a3a271c3422
Dean Herringer <deanher@gmail.com>
2019-07-28 11:16:11 +0200

    add data annotation for model validation

--- api/Cryptozon.Api/Models/PurchaseRequest.cs
+++ api/Cryptozon.Api/Models/PurchaseRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace Cryptozon.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cryptozon.Api.Models
 {
   public class PurchaseRequest
   {
+    [Required]
     public int CoinId { get; set; }
+
+    [Required]
     public decimal Quantity { get; set; }
+
+    [Required]
     public decimal UnitPrice { get; set; }
   }
 }
